c76eaf637d896bade3f703f06107c15413f94ad7
Cesar Galvis León <cgalvisleon@gmail.com>
2020-07-30 10:06:51 -0500

    Backup

--- src/models/prints.js
+++ src/models/prints.js
@@ -12,9 +12,10 @@ class Model {
     project_id = project_id || "";
     _class = _class || "-1";
     single = single || false;
-    console.log(project_id);
     if (project_id === "") {
       return respond(200, {}, 400, "Projecto requerido - /prints/<project_id>");
+    } else if (_class === "-1") {
+      return respond(200, {}, 400, "Clase requerida - /prints/<project_id>?_class=<_clase>&single=false");
     } else {
       const query = "SELECT * FROM js_core.GET_CLASS_PRINTS($1, $2, $3) RESULT";
       const params = [project_id, _class, single];
--- src/routes/prints.js
+++ src/routes/prints.js
@@ -10,7 +10,7 @@ function Api(app) {
 
   const service = new Service();
 
-  router.get("/:id", async function (req, res, next) {
+  router.get("/:project_id", async function (req, res, next) {
     const { project_id } = req.params;
     const { _class } = req.query;
     const { single } = req.query;
