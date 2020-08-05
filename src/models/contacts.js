6f902a514929b55fc401cfbdbfea290cb89fa272
Cesar Galvis León <cgalvisleon@gmail.com>
2020-08-05 08:48:58 -0500

    Deploy

--- src/models/contacts.js
+++ src/models/contacts.js
@@ -1,53 +1,50 @@
 const PgLib = require("../lib/postgresql");
-const { getValue, getDateTime, respond } = require("../lib/utilities");
+const { getValue, genId, respond } = require("../lib/utilities");
 const { MSG0004 } = require("../lib/msg");
 
 class Model {
   constructor(params) {
     this.db = new PgLib();
     this.params = params;
-    this._id = getValue(this.params, "_id", "-1");
   }
 
-  scheme(data) {
+  scheme() {
     const now = new Date();
     return {
-      date_make: getDateTime(data, "date_make", now),
-      date_update: getDateTime(data, "date_update", now),
-      _class: getValue(data, "_class", "CONTACT"),
-      _state: getValue(data, "_state", "0"),
-      _id: getValue(data, "_id", "-1"),
-      caption: getValue(data, "caption", ""),
-      description: getValue(data, "description", ""),
-      cellphone: getValue(data, "cellphone", ""),
-      phone: getValue(data, "phone", ""),
-      email: getValue(data, "email", ""),
-      country_id: getValue(data, "country_id", "-1"),
-      city_id: getValue(data, "city_id", "-1"),
-      city: getValue(data, "city", ""),
-      address: getValue(data, "address", ""),
-      identification_tp: getValue(data, "identification_tp", "-1"),
-      identification_type: getValue(data, "identification_type", ""),
-      identification: getValue(data, "identification", ""),
-      project_id: getValue(data, "project_id", "-1"),
-      profile_tp: getValue(data, "profile_tp", "-1"),
-      profile: getValue(data, "profile", ""),
-      avatar: getValue(data, "avatar", ""),
-      head_foto: getValue(data, "head_foto", ""),
-      _data: getValue(data, "_data", {}),
-      _v: getValue(data, "_v", 0),
-      user_id: getValue(data, "user_id", "-1"),
-      username: getValue(data, "username", ""),
+      date_make: now,
+      date_update: now,
+      _class: "CONTACT",
+      _state: "0",
+      _id: genId("-1"),
+      caption: "",
+      description: "",
+      cellphone: "",
+      phone: "",
+      email: "",
+      country_id: "-1",
+      city_id: "-1",
+      city: "",
+      address: "",
+      identification_tp: "-1",
+      identification_type: "",
+      identification: "",
+      project_id: "-1",
+      profile_tp: "-1",
+      profile: "",
+      avatar: "",
+      head_foto: "",
+      _data: {},
+      _v: 1,
+      user_id: getValue(this.params, "user_id", "-1"),
     };
   }
 
-  async get(project_id) {
-    if (this._id === "-1" || this._id === "new" || this._id === "") {
-      this.params["_id"] = "-1";
-      return respond(200, this.scheme(this.params));
+  async get(_id, project_id) {
+    if (_id === "-1" || _id === "new") {
+      return respond(200, this.scheme());
     } else {
       const query = "SELECT * FROM js_core.GET_CONTACT($1, $2) RESULT";
-      const params = [this._id, project_id];
+      const params = [_id, project_id];
       return await this.db
         .get(query, params)
         .then((result) => {
@@ -72,6 +69,7 @@ class Model {
       return respond(200, {}, 400, "Perfil requerido");
     } else {
       const _class = getValue(this.params, "_class", "");
+      const _id = getValue(this.params, "_id", "-1");
       const description = getValue(this.params, "description", "");
       const cellphone = getValue(this.params, "cellphone", "");
       const phone = getValue(this.params, "phone", "");
@@ -86,7 +84,7 @@ class Model {
       const query = "SELECT * FROM js_core.SET_CONTACT($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) RESULT";
       const params = [
         _class,
-        this._id,
+        _id,
         caption,
         description,
         cellphone,
@@ -115,9 +113,9 @@ class Model {
     }
   }
 
-  async state(project_id, _state) {
+  async state(_id, project_id, _state) {
+    _id = _id || "-1";
     _state = _state || "0";
-    const _id = this._id;
     const query = "SELECT * FROM js_core.STATE_CONTACT($1, $2, $3) RESULT";
     const params = [_id, project_id, _state];
     return await this.db
